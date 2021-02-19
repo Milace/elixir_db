277174c9bc63ecd742622bcd25ec551f784c46ff
Milthon Laureano Ceballos <mlceballos@personalsoft.com.co>
2021-02-19 16:00:13 -0500

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,45 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :curso_elixir_db,
+  ecto_repos: [CursoElixirDb.Repo]
+
+# Configures the endpoint
+config :curso_elixir_db, CursoElixirDbWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "+wJoDillDuFcueXxE7A1TiCkU4sZYBjXnzoPbNMrNkLwLz4zMh9C4iPGML3dU7hu",
+  render_errors: [view: CursoElixirDbWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: CursoElixirDb.PubSub,
+  live_view: [signing_salt: "twgw5iQy"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
+
+config :logger, level: :debug
+
+config :curso_elixir_db, CursoElixirDb.Scheduler,
+  jobs: [
+    # application_task: [
+    #   schedule: {:extended, "* * * * *"},
+    #   task: {JobQuantum, :print_text, ["tmp/increment.txt", "Hola "]}
+    # ],
+    application_task_insert: [
+      schedule: {:extended, "*/3 * * * *"},
+      task: {JobQuantum, :insert_data, []}
+    ]
+ ]
---
+++ lib/Quantum/job_quantum.ex
@@ -0,0 +1,41 @@
+defmodule JobQuantum do
+  @moduledoc """
+  This module contain funtions executed from cron jobs
+  """
+  import CursoElixirDb.HelperAccounts
+
+  @doc """
+   Crear y actualiza un archivo .txt en la ruta tmp/
+  """
+  def print_text(path, msg) do
+    fun = &(&1 + 1)
+    write = &(&2 |> File.write("#{&1}"))
+
+    if File.exists?(path) do
+      path |> File.read! |> String.to_integer |> fun.() |> printp(msg) |> write.(path)
+    else
+      path |> File.open!([:write]) |> File.close
+      path |> File.write("0")
+    end
+  end
+
+  defp printp(number, msg) do
+    IO.inspect(msg <> "#{number}")
+    number
+  end
+
+  @doc """
+   Inserta datos en la base de datos
+  """
+  def insert_data() do
+    create_accounts(%{type_document: "CC",
+                      num_document: 1113519071,
+                      email: "mlceballos@personalsoft.com.co",
+                      name_company: "personalsoft",
+                      name: "Milthon",
+                      last_name: "Ceballos"
+                    })
+  end
+
+
+end

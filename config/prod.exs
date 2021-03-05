4697d7080f289aafa477fc460743111a533d9c07
Milthon Ceballos <mlceballos@personalsoft.com.co>
2021-03-05 16:36:25 -0500

    add cross origin request and create deploy.yml file

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :curso_elixir_db, CursoElixirDbWeb.Endpoint,
   url: [host: "localhost", port: 4000],
+  check_origin: ["//localhost", "//64.225.4.18"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
